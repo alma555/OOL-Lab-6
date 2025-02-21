24d76b3fa71348695ca5974bb09636edfebbd015
Alma Thompson <ogburnstella@gmail.com>
2025-02-20 19:52:21 -0500

    Update GiftCardPayment.java

--- GiftCardPayment.java
+++ GiftCardPayment.java
@@ -1,6 +1,6 @@
 public class GiftCardPayment implements PaymentStrategy{
     @Override
     public void processPayment(double amount) {
-
+        System.out.println("Paid $" + amount + " with a gift card");
     }
 }
