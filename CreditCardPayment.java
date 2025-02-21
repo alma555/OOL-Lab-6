8757049f0fa016acfcc88e402329bd4871a40534
iceage2114 <31936203+iceage2114@users.noreply.github.com>
2025-02-20 19:42:14 -0500

    Update CreditCardPayment.java

--- CreditCardPayment.java
+++ CreditCardPayment.java
@@ -1,6 +1,10 @@
 public class CreditCardPayment implements PaymentStrategy{
     public String credCardName;
 
+    public CreditCardPayment(String name) {
+        this.credCardName = name;
+    }
+    
     @Override
     public void processPayment(double amount) {
 
