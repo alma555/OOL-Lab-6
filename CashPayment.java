3cb1e99826cd3986343c4f3d6cad2683e7a549e1
Alma Thompson <ogburnstella@gmail.com>
2025-02-20 19:51:31 -0500

    Update CashPayment.java

--- CashPayment.java
+++ CashPayment.java
@@ -1,6 +1,6 @@
 public class CashPayment implements PaymentStrategy {
     @Override
     public void processPayment(double amount) {
-
+        System.out.println("Paid $" + amount + " with cash");
     }
 }
